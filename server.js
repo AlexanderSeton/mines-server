67564a29b5409256fb2c26a238eac3ac977df1af
Alexander Seton <alexanderseton@protonmail.com>
2021-12-17 16:31:39 +0000

    redeplpoy 4

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const { Client } = require("pg");
 // })
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: "postgres://bsmzwnkjsacwhz:22e8702e00477bf50002fbead3902c49bdcb73862c1a5697938fb931bafccaaa@ec2-34-247-118-233.eu-west-1.compute.amazonaws.com:5432/dc58nbgq3458fk",
     ssl: {
       rejectUnauthorized: false
     }
