f315ffdba8714879e4c8b51c3f25f0c81de9b384
Alexander Seton <alexanderseton@protonmail.com>
2021-12-17 17:00:56 +0000

    redeploy 6

--- server.js
+++ server.js
@@ -14,33 +14,43 @@ const { Client } = require("pg");
 //     "database": "game"
 // })
 
-const client = new Client ({
-    "user": "bsmzwnkjsacwhz",
-    "password": "22e8702e00477bf50002fbead3902c49bdcb73862c1a5697938fb931bafccaaa",
-    "host": "ec2-34-247-118-233.eu-west-1.compute.amazonaws.com",
-    "database": "dc58nbgq3458fk",
-    "port": 5432,
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true, // This will help you. But you will see new error
-            rejectUnauthorized: false // This line will fix new error
-        }
-    }
-});
-
-// const client = new Client({
-//     connectionString: "postgres://bsmzwnkjsacwhz:22e8702e00477bf50002fbead3902c49bdcb73862c1a5697938fb931bafccaaa@ec2-34-247-118-233.eu-west-1.compute.amazonaws.com:5432/dc58nbgq3458fk",
-//     ssl: {
-//       rejectUnauthorized: false
+// const client = new Client ({
+//     "user": "bsmzwnkjsacwhz",
+//     "password": "22e8702e00477bf50002fbead3902c49bdcb73862c1a5697938fb931bafccaaa",
+//     "host": "ec2-34-247-118-233.eu-west-1.compute.amazonaws.com",
+//     "database": "dc58nbgq3458fk",
+//     "port": 5432,
+//     dialect: "postgres",
+//     dialectOptions: {
+//         ssl: {
+//             require: true, // This will help you. But you will see new error
+//             rejectUnauthorized: false // This line will fix new error
+//         }
 //     }
 // });
 
+const client = new Client({
+    connectionString: "postgres://bsmzwnkjsacwhz:22e8702e00477bf50002fbead3902c49bdcb73862c1a5697938fb931bafccaaa@ec2-34-247-118-233.eu-west-1.compute.amazonaws.com:5432/dc58nbgq3458fk",
+    ssl: {
+      rejectUnauthorized: false
+    }
+});
+
 client.connect()
 
+// app.get("/api/scores", (req, res) => {
+//     client.query("SELECT * FROM high_scores;")
+//     .then(results => res.send(results.rows));
+// })
+
 app.get("/api/scores", (req, res) => {
-    client.query("SELECT * FROM high_scores;")
-    .then(results => res.send(results.rows));
+    client.query('SELECT * FROM high_scores;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+          console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
 })
 
 app.post("/api/scores", (req, res) => {
@@ -50,4 +60,4 @@ app.post("/api/scores", (req, res) => {
     .then(result => res.send(result.rows))
     })
 
-app.listen(5432, () => console.log(`Listening on port`));
+app.listen(5432, () => console.log("Listening on port"));
