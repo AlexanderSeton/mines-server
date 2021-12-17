f59e8a333d887e1fe43e45542e5def335764e531
Alexander Seton <alexanderseton@protonmail.com>
2021-12-17 17:43:47 +0000

    testing simple api

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-
 const { Pool } = require('pg')
 
 const connectionString = "postgres://bsmzwnkjsacwhz:22e8702e00477bf50002fbead3902c49bdcb73862c1a5697938fb931bafccaaa@ec2-34-247-118-233.eu-west-1.compute.amazonaws.com:5432/dc58nbgq3458fk"
 
 const pool = new Pool({
   connectionString: connectionString,
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 module.exports = { pool }
--- index.js
+++ index.js
@@ -1,47 +1,12 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { pool } = require('./config')
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-const app = express()
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-
-const getScores = (request, response) => {
-  pool.query('SELECT * FROM high_scores', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-const addScore = (request, response) => {
-  const { author, title } = request.body
-  let p_name = request.body.player_name;
-  let p_score = request.body.score;
-
-  pool.query(
-    `INSERT INTO high_scores (player_name, score) VALUES ('${p_name}', ${p_score}) RETURNING *;`,
-    (error) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).json({ status: 'success', message: 'Score added.' })
-    }
-  )
-}
-
-app
-  .route('/api/scores')
-  // GET endpoint
-  .get(getScores)
-  // POST endpoint
-  .post(addScore)
-
-// Start server
-app.listen(5432, () => {
-  console.log(`Server listening`)
-})
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
