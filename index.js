a90b5ab5f2ee08d55d35c3cc972c01a3e0b65863
Alexander Seton <alexanderseton@protonmail.com>
2021-12-18 20:29:35 +0000

    fix 10

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ express()
       // const client = await pool.connect();
       // const result = await client.query(`INSERT INTO high_scores (player_name, score) VALUES ("${req.body.player_name}", "${req.body.score}") RETURNING *;`);
       // const results = { 'results': (result) ? result.rows : null};
-      res.send(req.body);
+      res.json(req.body);
       // client.release();
     } catch (err) {
       console.error(err);
