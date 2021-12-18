a47e753d70720ffb2df5f927fd43f8e0f8ed436f
Alexander Seton <alexanderseton@protonmail.com>
2021-12-18 03:19:57 +0000

    Added post root

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ express()
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM high_scores');
       const results = { 'results': (result) ? result.rows : null};
-      // res.render('pages/db', results);
+      res.send(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .post("api/scores", async (req, res) => {
+    try {
+      let p_name = req.body.player_name;
+      let p_score = req.body.score;
+      const client = await pool.connect();
+      const result = await client.query(`INSERT INTO high_scores (player_name, score) VALUES ('${p_name}', ${p_score}) RETURNING *;`);
+      const results = { 'results': (result) ? result.rows : null};
       res.send(results);
       client.release();
     } catch (err) {
