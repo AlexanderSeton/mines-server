75dbd23310c6845be32ee8b2610ce0977e504774
Alexander Seton <alexanderseton@protonmail.com>
2021-12-19 22:28:50 +0000

    fix 12

--- index.js
+++ index.js
@@ -23,16 +23,17 @@ express()
     // check if request is actually being processed !!!
     // let p_name = req.body.player_name;
     // let p_score = req.body.score;
-    try {
-      const client = await pool.connect();
-      const result = await client.query(`INSERT INTO "high_scores" (player_name, score) VALUES ('${req.body.player_name}', '${req.body.score}') RETURNING *;`);
-      // const results = { 'results': (result) ? result.rows : null};
-      res.send("hello world");
-      // client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+    res.send(req.body);
+    // try {
+    //   const client = await pool.connect();
+    //   const result = await client.query(`INSERT INTO "high_scores" (player_name, score) VALUES ('${req.body.player_name}', '${req.body.score}') RETURNING *;`);
+    //   // const results = { 'results': (result) ? result.rows : null};
+    //   res.send("hello world");
+    //   // client.release();
+    // } catch (err) {
+    //   console.error(err);
+    //   res.send("Error " + err);
+    // }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
