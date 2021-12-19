d8fabb5bbc4aa57ac1bc280eb48a2674f998793b
Alexander Seton <alexanderseton@protonmail.com>
2021-12-19 22:36:04 +0000

    fix 15

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ express()
     // let p_name = req.body.player_name;
     // let p_score = req.body.score;
     try {
-      const client = await pool.connect();
-      const result = await client.query(`INSERT INTO high_scores (player_name, score) VALUES ('${req.body.player_name}', ${req.body.score}) RETURNING *;`);
-      const results = { 'results': (result) ? result.rows : null};
-      res.send(results);
+      // const client = await pool.connect();
+      // const result = await client.query(`INSERT INTO high_scores (player_name, score) VALUES ('${req.body.player_name}', ${req.body.score}) RETURNING *;`);
+      // const results = { 'results': (result) ? result.rows : null};
+      res.send(req.json());
       // client.release();
     } catch (err) {
       console.error(err);
